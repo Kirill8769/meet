c5f0f2a68051abca050508278f1b95b193fe6699
Kirill8769 <tkkrozn@gmail.com>
2023-10-28 16:20:42 +0300

    add get str list function

--- task_2.py
+++ task_2.py
@@ -1,2 +1,12 @@
-def foo():
-    pass
+def get_str_list(str_list: list) -> list:
+    """
+    Функция получает на вход список строк
+    и возвращает новый список, содержащий только уникальные строки
+
+    :param str_list: список строк
+    :return: уникальные строки
+    """
+    return sorted(set(str_list))
+
+
+print(get_str_list(['apple', 'banana', 'orange', 'apple']))
