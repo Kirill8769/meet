096e4b2f8a6879d1bdc4ed9441e7d8cbd56955a9
Kirill8769 <tkkrozn@gmail.com>
2023-10-28 16:35:51 +0300

    add file task_3 and add get list films function

---
+++ task_3.py
@@ -0,0 +1,32 @@
+def get_list_films(list_dict: list, genre: str) -> list:
+    """
+    Функция получает на вход список словарей,
+    содержащих информацию о фильмах (например, название, жанр, режиссер и т.д.),
+    и возвращает новый список, содержащий только те фильмы,
+    которые относятся к заданному жанру.
+    :param list_dict: список словарей, содержащих информацию о фильмах
+    :param genre: Жанр
+    :return: Возвращает новый список,
+    содержащий только те фильмы, которые относятся к заданному жанру.
+    """
+    result = [film for film in list_dict if film['genre'] == genre]
+    return result
+
+
+print(get_list_films([
+    {
+        'title': 'The Shawshank Redemption',
+        'genre': 'Drama',
+        'director': 'Frank Darabont'
+     },
+    {
+        'title': 'The Godfather',
+        'genre': 'Crime',
+        'director': 'Francis Ford Coppola'
+    },
+    {
+        'title': 'The Dark Knight',
+        'genre': 'Action',
+        'director': 'Christopher Nolan'
+    }
+], genre='Drama'))
