726bcda2f724bea459890197e566a5e71bf0729d
Kirill8769 <tkkrozn@gmail.com>
2023-10-28 17:05:04 +0300

    add task_4, add get double int function

--- task_3.py
+++ task_3.py
@@ -1,32 +1,26 @@
-def get_list_films(list_dict: list, genre: str) -> list:
+def get_list_films(list_films: list, genre: str) -> list:
     """
     Функция получает на вход список словарей,
     содержащих информацию о фильмах (например, название, жанр, режиссер и т.д.),
     и возвращает новый список, содержащий только те фильмы,
     которые относятся к заданному жанру.
-    :param list_dict: список словарей, содержащих информацию о фильмах
+
+    :param list_films: список словарей, содержащих информацию о фильмах
     :param genre: Жанр
     :return: Возвращает новый список,
     содержащий только те фильмы, которые относятся к заданному жанру.
     """
-    result = [film for film in list_dict if film['genre'] == genre.title()]
+    result = [film for film in list_films if film["genre"] == genre.title()]
     return result
 
 
-print(get_list_films(list_dict=[
-    {
-        'title': 'The Shawshank Redemption',
-        'genre': 'Drama',
-        'director': 'Frank Darabont'
-     },
-    {
-        'title': 'The Godfather',
-        'genre': 'Crime',
-        'director': 'Francis Ford Coppola'
-    },
-    {
-        'title': 'The Dark Knight',
-        'genre': 'Action',
-        'director': 'Christopher Nolan'
-    }
-], genre='drama'))
+print(
+    get_list_films(
+        list_films=[
+            {"title": "The Shawshank Redemption", "genre": "Drama", "director": "Frank Darabont"},
+            {"title": "The Godfather", "genre": "Crime", "director": "Francis Ford Coppola"},
+            {"title": "The Dark Knight", "genre": "Action", "director": "Christopher Nolan"},
+        ],
+        genre="drama",
+    )
+)
